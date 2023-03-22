26b8c7b03594fb7d818fbabb9eee2f066bbc7ca4
souta <gwxsouta@gmail.com>
2023-03-22 15:05:44 +0900

    タイトル、挨拶を修正

--- js/controller.js
+++ js/controller.js
@@ -2,4 +2,6 @@ function doReply(){
     alert('Hi!');
     var inputText = document.getElementById('inputText');
     console.log(inputText.value);
+    var msgArea = document.getElementById('msgArea');
+    msgArea.innerText = inputText.value;
 }
